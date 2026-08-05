d335a67b4a3de946ecc721c70a6c5c087324dd2e
tosuki <okaabe2006@gmail.com>
2026-08-05 18:48:24 -0300

    refactor: update SaveConfig signature to accept pathName parameter

--- linux/domain/core/config.loader.go
+++ linux/domain/core/config.loader.go
@@ -5,5 +5,5 @@ import "github.com/4mti/ponto/domain/model"
 type ConfigLoader interface {
 	LoadConfig(pathName string) (*model.Config, error)
 	LoadConfigOrElse(pathName string, orElse *model.Config) *model.Config
-	SaveConfig(config *model.Config) error
+	SaveConfig(config *model.Config, pathName string) error
 }
